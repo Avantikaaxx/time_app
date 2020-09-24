61a3fc20183673b8fc546a15aaa4bbbf13deba80
Avantikaaxx <57575449+Avantikaaxx@users.noreply.github.com>
2020-09-24 04:47:54 -0400

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from datetime import datetime
 app = Flask(_name_)
 
 @app.route('/time')
-def time_app():
+def timeapp():
     dt = datetime.now()
     s = "Hello Professor Franchitti! you visited the website at: " + str(dt)
     return(s)
